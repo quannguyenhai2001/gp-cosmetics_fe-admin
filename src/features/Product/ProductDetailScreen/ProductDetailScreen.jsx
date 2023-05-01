fab54ae42e40342f032d9881d0ec4f8340e9d91d
quannguyenhai2001 <quanbn134679258@gmail.com>
2023-05-01 23:24:35 +0700

    feat: add prduct detail screen

--- src/features/Product/ProductDetailScreen/ProductDetailScreen.jsx
+++ src/features/Product/ProductDetailScreen/ProductDetailScreen.jsx
@@ -1,5 +1,5 @@
 
-import { Container, Grid, Stack, Typography } from '@mui/material';
+import { Box, Container, Grid, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -34,43 +34,125 @@ const ProductDetailScreen = () => {
             <Typography variant="h2" fontWeight="bold" fontSize="30px" mb={30}>
                 Chi tiết sản phẩm
             </Typography>
-            <Stack direction="column" spacing={10} >
-                <Grid
-                    container
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                    }}
-                >
-                    <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
-                        Tên nhà cung cấp :
+            {Object.keys(userDetail).length > 0 ? (
+                <Stack direction="column" spacing={10} >
+                    <Grid
+                        container
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
+                            Tên sản phẩm :
+                        </Grid>
+                        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                            <Typography>
+                                {userDetail?.product_name}
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
-                        <Typography>
-                            {userDetail?.name}
-                        </Typography>
+                    <Grid
+                        container
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
+                            Tên nhà cung cấp :
+                        </Grid>
+                        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                            <Typography>
+                                {userDetail?.manufacturer_name}
+                            </Typography>
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Grid
-                    container
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                    }}
-                >
-                    <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
-                        Địa chỉ :
+                    <Grid
+                        container
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
+                            Giá :
+                        </Grid>
+                        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                            <Typography>
+                                {userDetail?.price}
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
-                        <Typography>
-                            {userDetail?.address}
-                        </Typography>
+                    <Grid
+                        container
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
+                            Giảm giá :
+                        </Grid>
+                        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                            <Typography>
+                                {userDetail?.promotion}%
+                            </Typography>
+                        </Grid>
                     </Grid>
-                </Grid>
+                    <Grid
+                        container
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
+                            Đánh giá trung bình :
+                        </Grid>
+                        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                            <Typography>
+                                {userDetail?.rating ? (<>{userDetail?.rating}/5</>) : (<>Không</>)}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        container
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
+                            Ảnh minh họa :
+                        </Grid>
+                        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                            <Box sx={{ height: 100 }}>
+                                <img style={{ height: "100%" }} src={userDetail.thumbnail_url} alt="cat" />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        container
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Grid item xs={4} sm={4} md={4} lg={2} xl={4}>
+                            Bộ sưu tập ảnh :
+                        </Grid>
+                        <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
+                            <Box sx={{ height: 120, width: "100%", overflowX: "scroll", display: "flex" }}>
+                                {JSON.parse(userDetail.gallery_image_urls).map((item, index) => <img key={index} style={{ height: "100%", marginRight: 10 }} src={item} alt="cat" />)}
+                            </Box>
+                        </Grid>
+                    </Grid>
 
 
 
-            </Stack>
+                </Stack>
+            ) : (<>Lỗi</>)}
         </Container >
     );
 };
